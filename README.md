fc26abce0687d36afc62062efb86bae5e9692717
Andrés Bustamante <afbusta@yahoo.com>
2017-02-25 16:08:26 +0100

    Added reCAPTCHA validation before sending messages

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Made for Java EE 6
 It uses : 
 - JavaServer Faces 2.1
 - JavaMail 1.4
+- reCAPTCHA
+- Apache HttpComponents 4.5.2
--- src/main/java/net/andresbustamante/mail/contact/web/MailContactBean.java
+++ src/main/java/net/andresbustamante/mail/contact/web/MailContactBean.java
@@ -1,16 +1,30 @@
 package net.andresbustamante.mail.contact.web;
 
 import net.andresbustamante.mail.contact.services.MailService;
+import net.andresbustamante.mail.contact.util.ConfigProperties;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonReader;
 import javax.mail.MessagingException;
-import java.io.Serializable;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 /**
@@ -54,9 +68,30 @@ public class MailContactBean implements Serializable {
         messages = ctx.getApplication().getResourceBundle(ctx, "messages");
     }
 
+    /**
+     *
+     * @return
+     */
     public String send() {
-        log.info("A new message is going to be sent");
         FacesMessage response;
+        String captchaResponse = ctx.getExternalContext().getRequestParameterMap().get("g-recaptcha-response");
+
+        if (isCaptchaValid(captchaResponse)) {
+            sendMessage();
+        } else {
+            response = new FacesMessage(FacesMessage.SEVERITY_WARN, messages.getString("email.not.sent"),
+                    messages.getString("email.not.sent"));
+            ctx.addMessage(null, response);
+        }
+        return "result";
+    }
+
+    /**
+     *
+     */
+    private void sendMessage() {
+        FacesMessage response;
+        log.info("A new message is going to be sent");
 
         try {
             mailService.sendMail(subject, content, name, email);
@@ -70,7 +105,76 @@ public class MailContactBean implements Serializable {
                     e.getMessage());
             ctx.addMessage(null, response);
         }
-        return "result";
+    }
+
+    /**
+     *
+     * @param captchaResponse
+     * @return
+     */
+    private boolean isCaptchaValid(String captchaResponse) {
+        if (captchaResponse != null && !captchaResponse.isEmpty()) {
+            try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+                HttpPost httpPost = new HttpPost(ConfigProperties.getValue("recaptcha.verify.url"));
+                List<NameValuePair> parameters = new ArrayList<>();
+                parameters.add(new BasicNameValuePair("secret", ConfigProperties.getValue("recaptcha.secret.key")));
+                parameters.add(new BasicNameValuePair("response", captchaResponse));
+                httpPost.setEntity(new UrlEncodedFormEntity(parameters));
+
+                if (isCaptchaValid(httpClient, httpPost)) return true;
+            } catch (IOException e) {
+                log.error("An error occurred when validating captcha input.", e);
+            }
+        }
+        return false;
+    }
+
+    /**
+     *
+     * @param httpClient
+     * @param httpPost
+     * @return
+     * @throws IOException
+     */
+    private boolean isCaptchaValid(CloseableHttpClient httpClient, HttpPost httpPost) throws IOException {
+        try (CloseableHttpResponse response = httpClient.execute(httpPost)) {
+            log.info("Captcha validation status : " + response.getStatusLine());
+
+            HttpEntity captchaEntity = response.getEntity();
+
+            if (captchaEntity != null && captchaEntity.getContent() != null) {
+                Boolean verified = isCaptchaValid(captchaEntity);
+                EntityUtils.consume(captchaEntity);
+
+                log.info("Validated by captcha ? " + verified);
+                return verified;
+            }
+        }
+        return false;
+    }
+
+    /**
+     *
+     * @param captchaEntity
+     * @return
+     * @throws IOException
+     */
+    private boolean isCaptchaValid(HttpEntity captchaEntity) throws IOException {
+        StringBuilder response = new StringBuilder();
+
+        // Read the response from captcha server
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(captchaEntity.getContent()))) {
+            String input;
+
+            while ((input = in.readLine()) != null) {
+                response.append(input);
+            }
+        }
+
+        // Read the Json object with the validation result
+        try (JsonReader reader = Json.createReader(new StringReader(response.toString()))) {
+            return reader.readObject().getBoolean(ConfigProperties.getValue("recaptcha.verify.property"));
+        }
     }
 
     public String getName() {
